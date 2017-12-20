8075fd322a9171b4abf43982051ad58631e9dc03
pydeepak <deepakkumar.dubey@cuelogic.com>
2017-12-20 13:10:47 +0530

    initil commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='dj-cuelogic',
+    version='0.2',
+    description='Django utility modules',
+    author='Deepak Kumar Dubey',
+    author_email='deepakdubey597@gmail.com',
+    url='https://deepakdubey.tech/',
+    packages=find_packages(),
+    zip_safe=False,
+)
